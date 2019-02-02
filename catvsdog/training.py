71bbc5ec5a5a6bc3875373bacde1002f1908595b
Hao Bai <baihaozi12@gmail.com>
2019-02-02 17:39:49 +0000

    c

--- catvsdog/training.py
+++ catvsdog/training.py
@@ -18,7 +18,27 @@ learing_rate = 0.0001
 def run_training():
     train_dir = 'train'
     logs_train_dir = 'logs/train'
-    
+
     train, train_label = input_data.gey_files(train_dir)
 
+    train_batch,train_label_batch = input_data.get_batch(train,
+                                                         train_label,
+                                                         IMG_W,
+                                                         IMG_H,
+                                                         BATCH_SIZE,
+                                                         CAPACITY)
+    train_logits = model.inference(train_batch, BATCH_SIZE, N_CLASSES)
+    train_loss = model.losses(train_logits, train_label_batch)
+    train_op = model.trainning(train_loss, learing_rate)
+    train_acc = model.evaluation(train_logits, train_label_batch)
+
+    summary_op = tf.summary.merge_all()
+    sess = tf.Session()
+    train_writer = tf.summary.FileWriter(logs_train_dir, sess.graph)
+    saver = tf.train.Saver()
+
+    sess.run(tf.global_variables_initializer())
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess = sess, coord=coord)
+    
 
